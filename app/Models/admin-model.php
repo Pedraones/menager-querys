c564494c8dc33ec24d506627547a18894f4f0ba4
Pedro Augusto <perobr2018@gmail.com>
2026-08-05 15:12:00 -0300

    feat - Creation class for insert new pisition interprise

---
+++ app/Models/admin-model.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . "/secrets.php";
+
+use const app\Models\secrets\DB;
+use const app\Models\secrets\PORT;
+use const app\Models\secrets\USER;
+use const app\Models\secrets\PASSWORD;
+use const app\Models\secrets\HOST;
+use const app\Models\secrets\TABLE;
+
+class adminPosition{
+
+   public function addPosition($name){
+      $db = new mysqli(
+         HOST,
+         USER,
+         PASSWORD,
+         DB,
+         PORT
+      );
+
+      $building_query = $db->prepare("INSERT INTO " . TABLE . " (name) VALUES (?)");
+      $building_query->bind_param("s", $name);
+      $building_query->execute();
+
+      $db->close();
+   }
+
+}
+?>
