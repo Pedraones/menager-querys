62c0b05310bfd3f2d4b5e3d779d6a5eea196ba29
Pedro Augusto <perobr2018@gmail.com>
2026-08-18 17:53:09 -0300

    feat - Receivend datas from administrator page

---
+++ app/Controllers/admin-controller.php
@@ -0,0 +1,23 @@
+<?php
+$dir_root = getenv('dir_menager_querys');
+
+require_once $dir_root . 'app/Services/admin-service.php'; 
+require_once $dir_root . 'app/helpers/verifications.php';
+require_once $dir_root . 'app/helpers/hash.php';
+
+class adminController{
+   public function receiveParamsAddPosition(...$params): array{
+      $verify = new Verifications();
+      $allParamsBeenReceived = $verify->receivedAllParamsToAddPosition($params[0]);
+
+      if($allParamsBeenReceived == false) return ["success" => false];
+
+      $result = [
+         "success" => true,
+         "data" => $params[0]["name"]
+      ];
+
+      return $result;
+   }
+}
+?>
