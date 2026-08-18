f03427fe77b123b57979ba0d9957b6e92bd3e14c
Pedro Augusto <perobr2018@gmail.com>
2026-08-18 19:15:47 -0300

    fix - Adjusting name of variables

--- app/helpers/ensure-idempotence.php
+++ app/helpers/ensure-idempotence.php
@@ -1,7 +1,7 @@
 <?php
 
    function ensureIdempotence(...$params): bool{
-      if($params[0]["new_value"] == $params[0]["old_value"]) return true;
+      if($params[0]["newData"] == $params[0]["oldData"]) return true;
 
       return false;
    }
