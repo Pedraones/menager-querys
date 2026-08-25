ad61472e594503398df9b45839820f761884d0af
Pedro Augusto <perobr2018@gmail.com>
2026-08-25 18:04:23 -0300

    feat - Created function to call all functions for add user

--- app/Routes.php
+++ app/Routes.php
@@ -26,6 +26,29 @@ class Routes{
       if($responseAdminService == false) return false;
       return true;
    }   
-}
 
+   public function addUser($user): bool{
+      $adminController = new adminController();
+
+      $responseAdminController = $adminController->receiveParamsAddUser($user);
+
+      var_dump($responseAdminController);
+      echo "<br> <br> <br>";
+
+      if($responseAdminController["success"] == false) return false;
+      
+      unset($adminController);
+
+      $adminService = new adminService();
+
+      $responseAdminService = $adminService->insertUser($user);
+      #var_dump($responseAdminService);
+      if($responseAdminService == false) return false;
+
+      unset($user);
+      unset($responseAdminService);
+
+      return true;
+   }
+}
 ?>
