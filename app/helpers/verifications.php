738d6be808eebbba1a741a221e48ee5f2c49a4f7
Pedro Augusto <perobr2018@gmail.com>
2026-08-25 16:13:16 -0300

    fix - Correction of pass of variable to comparision and name from your attribute

--- app/helpers/verifications.php
+++ app/helpers/verifications.php
@@ -12,21 +12,21 @@ class Verifications{
          $params[0]["newDatas"] == []) return false;
 
       $values = $params[0]["newDatas"];
+      
+      if($values["user"] == NULL ||
+         $values["user"] == "") return false;
 
-      if($values[0]["user"] == NULL ||
-         $values[0]["user"] == "") return false;
+      if($values["email"] == NULL ||
+         $values["email"] == "") return false;
 
-      if($values[0]["email"] == NULL ||
-         $values[0]["email"] == "") return false;
+      if($values["id_position_interprise"] == NULL ||
+         $values["id_position_interprise"] == "") return false;
 
-      if($values[0]["position_interprise"] == NULL ||
-         $values[0]["position_interprise"] == "") return false;
+      if($values["password"] == NULL ||
+         $values["password"] == "") return false;
 
-      if($values[0]["password"] == NULL ||
-         $values[0]["password"] == "") return false;
-
-      if($values[0]["salt"] != NULL &&
-         $values[0]["salt"] == "") return false;
+      if($values["salt"] != NULL &&
+         $values["salt"] == "") return false;
 
       return true;
    }
