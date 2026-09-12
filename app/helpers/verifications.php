fcb90caaf05b67f10497e2e0a61780e80c2aaa7d
Pedro Augusto <perobr2018@gmail.com>
2026-09-11 22:19:00 -0300

    remove - Removed lines used to realize tests

--- app/helpers/verifications.php
+++ app/helpers/verifications.php
@@ -35,53 +35,31 @@ class Verifications{
       if($values["newDatas"] == NULL ||
          $values["newDatas"] == []) return false;
 
-      echo "1";
-
       $query = $values["newDatas"];
       $quantityFields = count($query);
 
       if($quantityFields < 8) return false;
 
-      echo "2";
-
       if($query["id_user"] == NULL) return false;
-
-      echo "3";
 
       if($query["name"] == NULL ||
          $query["name"] == "") return false;
 
-      echo "4";
-
       if($query["file_query"] == NULL &&
          $query["content"] == NULL) return false;
-
-      echo "5";
 
       if($query["file_query"] == "" &&
          $query["content"] == "") return false;
 
-      echo "6";
-
       if($query["description"] == NULL ||
          $query["description"] == "") return false;
 
-      echo "7";
-
       if($query["public_view"] > 1 ) return false;
-
-      echo "8";
-
-      echo $query["referred"];
 
       if(substr_compare($query["referred"], "hdk", 1, 3) == 0 ||
          substr_compare($query["referred"], "esus", 1, 4) == 0) return false;
 
-      echo "9";
-
       if($query["code"] == NULL) return false;
-
-      echo "10";
 
       return true;
    }
