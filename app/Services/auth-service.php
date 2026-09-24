862bc8106e34a6f15719c990487bf012e45ebcdc
Pedro Augusto <perobr2018@gmail.com>
2026-09-23 21:35:45 -0300

    feat - Check if user with credentials exist

---
+++ app/Services/auth-service.php
@@ -0,0 +1,38 @@
+<?php
+$dir_root = getenv("dir_menager_querys");
+require_once $dir_root . "/app/helpers/hash.php";
+require_once $dir_root . "/app/helpers/structuring-responses.php";
+require_once $dir_root . "/app/Models/user-model.php";
+
+class authService{
+   public function login($credentials): bool{
+      $userModel = new userModel();
+      
+      $saltUser = $userModel->getSaltUser(["email" => $credentials["email"]]);
+      $saltUser = structureResponseGetSalt($saltUser);
+
+      unset($userModel);
+
+      $passwordEncrypt = encrypt([
+         "password" => $credentials["password"],
+         "salt" => $saltUser
+      ]);
+
+      var_dump($passwordEncrypt);
+
+      unset($saltUser);
+      $credentials["password"] = $passwordEncrypt;
+
+      $userModel = new userModel();
+      $userExist = $userModel->getLoginUser($credentials);
+
+      unset($credentials);
+      unset($passwordEncrypt);
+      unset($userModel);
+
+      if(!$userExist) return false;
+
+      return true;
+   }
+}
+?>
